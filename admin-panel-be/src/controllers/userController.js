c3de8e0c94a50e3394529dac471fc49f3b6d180f
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2025-08-29 22:37:38 +0530

    Created API to get and register the user

---
+++ admin-panel-be/src/controllers/userController.js
@@ -0,0 +1,25 @@
+import User from "../models/userModel";
+
+// @desc - Get all users
+// @route - GET /api/users
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// @desc - Create a new user
+// @route - POST /api/users
+
+export const createUser = async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
+}
